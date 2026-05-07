c43b07edba30274b3784badbdb86d6e9198f205e
Lucaskirschenbaum <50088602@est.ort.edu.ar>
2026-05-07 11:42:34 -0300

    La caja vuelve a su posición inicial

--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -6,11 +6,24 @@ public class CajaMovement : MonoBehaviour
 
 {
     public float velocidad;
-    
-
+    float initialYValue;
     // Update is called once per frame
+
+    void Start()
+    {
+        initialYValue = transform.position.y;
+    }
     void Update()
     {
         transform.Translate(0,-velocidad * Time.deltaTime, 0);
+        
+    }
+
+    public void MoveBoxToRandomPosition()
+    {
+        //Para probar, solamente hacemos que suba
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
     }
 }
